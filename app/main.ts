98c162ee3b6281ee2a3088990413272e3a37d16d
ephie <ephie@localhost.localdomain>
2017-07-07 17:57:04 +0300

    created the food and list component where food together with its details are dislayed

---
+++ app/main.ts
@@ -0,0 +1,8 @@
+
+//Responsible for loading the AppModule file
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
